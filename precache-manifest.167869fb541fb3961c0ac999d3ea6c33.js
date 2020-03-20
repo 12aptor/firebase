18f23d31ddad10fde3f9f88c5223ddf78a2ccec9
Paolo Soncco <paholo2054@gmail.com>
2020-03-20 11:06:54 -0500

    commit

---
+++ precache-manifest.167869fb541fb3961c0ac999d3ea6c33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896547779395205164a2ce681c5eae3e",
+    "url": "/firebase/index.html"
+  },
+  {
+    "revision": "6790ff1c3e340133bc60",
+    "url": "/firebase/static/css/main.6280febe.chunk.css"
+  },
+  {
+    "revision": "7fcf7ff4c5efe9365ab5",
+    "url": "/firebase/static/js/2.12d78ff6.chunk.js"
+  },
+  {
+    "revision": "fff893798278dc57ce07f24dd8c43a2b",
+    "url": "/firebase/static/js/2.12d78ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6790ff1c3e340133bc60",
+    "url": "/firebase/static/js/main.8c42f2cb.chunk.js"
+  },
+  {
+    "revision": "c5115824c33f3981d986",
+    "url": "/firebase/static/js/runtime-main.544d7626.js"
+  },
+  {
+    "revision": "c52c69467d157c2248480b66a5a2b4cf",
+    "url": "/firebase/static/media/calavera.c52c6946.jpg"
+  }
+]);
